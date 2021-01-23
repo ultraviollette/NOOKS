6c495e567177d9196353849c26c21698e74aa9b4
Melissa Lee <plhw90@gmail.com>
2021-01-22 23:58:26 -0700

    fourth commit

---
+++ useFullscreen/useFullscreen.js
@@ -0,0 +1,38 @@
+export const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullScreen) {
+        element.current.requestFullScreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullScreen) {
+        element.current.webkitRequestFullScreen();
+      } else if (element.current.msRequestFullScreen) {
+        element.current.msRequestFullScreen();
+      }
+      runCb(true);
+    }
+  };
+
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
